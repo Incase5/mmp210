5f3b57cf923dd873b1d2e43229b00eda195bb84d
Incase5 <jeremybaut2@hotmail.com>
2020-02-18 17:40:06 -0500

    update

--- variables/sketch.js
+++ variables/sketch.js
@@ -3,31 +3,42 @@ Jeremy Bautista
 Self portrait 
 */
 
-var x = 150;
-var y = 130;
+var x = 200;
+var y = 180;
 
-var s = 180;
+var s = 200;
+
+
 
 function setup() {
-	createCanvas(500, 360);
+	createCanvas(640, 360);
 }
 
 function draw() {
 	background("#9ACBD6");
-	
+	stroke("white");
+	strokeWeight(5);
 
 	//face
 	fill("#F3DCAB");
-	stroke("white");
-	strokeWeight(5);
-	ellipse(x, y, z);
-
+	
+	//ellipse(200, 180, 200, 250);
+	ellipse(x, y, s);
+	
 	//eyes 
 	fill("#946D1F");
-	ellipse(150, 150, 50, 100);
-	ellipse(250, 150, 40, 80);
+	ellipse(x - 50, y - 25, 50, s/2);	//left eye
+	ellipse(x + 50, y - 30, 40, s/2.5);	//right eye
 
 	//mouth
+	rectMode(CENTER);
 	stroke("#DCC38E");
-	line(150, 250, 200, 230);
+	strokeWeight(2);
+	noFill();
+	line(x - 50, 250, 200, 230);
+
+
+	// x = 200
+	// y = 180
+	// s = 200
 }
