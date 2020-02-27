a226f7c01c8b2dc34de9da6efe5f806a2c25c3f9
Incase5 <jeremybaut2@hotmail.com>
2020-02-27 16:48:40 -0500

    update

--- meme_1/sketch.js
+++ meme_1/sketch.js
@@ -4,6 +4,12 @@ Meme Version 1
 2/25/2020
 */
 
+var catImage;
+
+function preload(){
+	catImage = loadImage("cat.jpg");
+}
+
 function setup(){
 	var canvas = createCanvas(500, 500);
 	canvas.drawingContext.miterLimit = 1;
@@ -12,12 +18,16 @@ function setup(){
 function draw(){
 	background('skyblue');
 
+	//draw the image
+	image(catImage, 0, 0, 500, 500);
+	
+
 	textSize(100);
 	fill('blue');
 	stroke('purple');
 	strokeWeight('3');
 	textStyle(ITALIC);
-	text("Hello world", 0, 250);
+	text("Hello world", 0, 80);
 
 	textSize(45);
 	textStyle(NORMAL);
@@ -25,6 +35,6 @@ function draw(){
 
 	var welcome = 'Welcome to my website';
 	var end = map(frameCount, 0, 200, 0, welcome.length);
-	text(welcome.substring(0, end), 0, 350)
+	text(welcome.substring(0, end), 0, 480)
 
 }
