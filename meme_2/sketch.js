9403f959c85835cbe410d8e509352debda9bd7fa
Incase5 <jeremybaut2@hotmail.com>
2020-03-05 17:41:21 -0500

    update

--- meme_2/sketch.js
+++ meme_2/sketch.js
@@ -5,41 +5,65 @@ Meme Version 2
 */
 
 var kirbyImage;
+var kirbyShoot;
+var kirbyBang;
+var kirbyBoom;
+
+var counter = 0;
+var instructions = "click to shoot";
+var x;
+var y;
 
 function preload(){
 	kirbyImage = loadImage("kirby.jpg");
 	kirbyShoot = loadImage("bang.jpg");
-
+	kirbyBang = loadImage("kirbybang.jpg");
+	kirbyBoom = loadImage("kirboom.jpg");
 }
 
 function setup(){
 	var canvas = createCanvas(500, 500);
-	canvas.drawingContext.miterLimit = 1;
+	canvas.drawingContext.miterLimit = 2;
+}
+
+function mousePressed(){
+	counter++;
+	if (counter == 4) {
+		counter = 0;
+	}
+
+	if (mouseX > width/2 && mouseY > height/2) {
+		// bottom right
+		instructions = 'now shoot again';
+		y = 50;
+	} else if (mouseX > width/2 && mouseY < height/2) {
+		instructions = 'finish it';
+		x = 50;
+	} else if (mouseX < width/2 && mouseY < height/2) {
+		instructions = 'ACE';
+		y = height - 100;
+	}
 }
 
 function draw(){
 	background('skyblue');
 
 	//draw the image
-	if (mouseIsPressed) {
-		image(kirbyShoot, 0, 0, 500, 500);
-	} else {
+	if (counter == 0) {
 		image(kirbyImage, 0, 0, 500, 500);
+	} else if {
+		image(kirbyShoot, 0, 0, 500, 500);
+	} else if {
+		image(kirbyBang, 0, 0, 500, 500);
+	}  else {
+		image(kirbyBoom, 0, 0, 500, 500);
 	}
 	
-	
-
 	textSize(40);
 	fill('blue');
 	stroke('purple');
 	strokeWeight('3');
 	textStyle(ITALIC);
-if (mouseIsPressed){
-	text("BANG", 380, 260);
-}
-
-	//text("my face", 250, 460);
-
 	textSize(45);
 	textStyle(NORMAL);
 }
