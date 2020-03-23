5505c6cdb0193fcfbef06edaef5102b78395ae03
Incase5 <jeremybaut2@hotmail.com>
2020-03-23 11:06:10 -0400

    update

--- meme_2/sketch.js
+++ meme_2/sketch.js
@@ -9,6 +9,11 @@ var kirbyShoot;
 var kirbyBang;
 var kirbyBoom;
 
+var kirbSize;
+var kirbSizeSpeed = 5;
+
+var kirbX;
+
 var counter = 0;
 var instructions = "click to shoot";
 var x;
@@ -24,6 +29,12 @@ function preload(){
 function setup(){
 	var canvas = createCanvas(500, 500);
 	canvas.drawingContext.miterLimit = 2;
+
+	x = width - 150;
+	y = height - 100;
+
+	kirbSize = width;
+	kirbX = 0;
 }
 
 function mousePressed(){
@@ -50,10 +61,17 @@ function draw(){
 
 	//draw the image
 	if (counter == 0) {
+		//imageMode(CENTER);
 		image(kirbyImage, 0, 0, 500, 500);
-	} else if {
+
+		kirbSize += kirbSizeSpeed;
+		if (kirbSize > width * 2 || kirbSize < width - kirbSizeSpeed) {
+			kirbSizeSpeed *= -1;
+		}
+
+	} else if (counter == 1) {
 		image(kirbyShoot, 0, 0, 500, 500);
-	} else if {
+	} else if (counter == 2) {
 		image(kirbyBang, 0, 0, 500, 500);
 	}  else {
 		image(kirbyBoom, 0, 0, 500, 500);
