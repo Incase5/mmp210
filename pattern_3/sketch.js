87ea160ea12e25e3840d74ad5159b6b26cc0071a
Incase5 <jeremybaut2>
2020-05-10 19:28:16 -0400

    update

--- pattern_3/sketch.js
+++ pattern_3/sketch.js
@@ -3,35 +3,25 @@ Jeremy Bautista
 Pattern Version 3
 */
 
-var columnSlider;
-var greenSlider;
-var eyeSlider;
-var circleSlider;
+var wSlider;
+var heightSlider;
 
 function setup() {
 	createCanvas(640, 360);
 	noStroke();
 
-	createP("Column width");
-	columnSlider = createSlider(20, 200, 100);
-	columnSlider.input(pattern);
+	createP("Cell size");
+	wSlider = createSlider(1, 100, 40);
+	wSlider.input(pattern);
 
-	createP("Green color");
-	greenSlider = createSlider(0, 255, 200);
-	greenSlider.input(pattern);
-
-	createP("Eye");
-	eyeSlider = createSlider(5, 50, 5);
-	eyeSlider.input(pattern);
-
-	createP("Circles");
-	circleSlider = createSlider(40, 200, 40);
-	circleSlider.input(pattern);
-
-	var patternButton = createButton("New Pattern");
+	createP("Height");
+	heightSlider = createSlider(-10, height, 300);
+	heightSlider.input(pattern);
+	
+	var patternButton = createButton("Pattern");
 	patternButton.mousePressed(pattern);
 
-	var saveButton = createButton("Save Image");
+	var saveButton = createButton("Save");
 	saveButton.mousePressed(saveImage);
 
 	pattern();
@@ -42,39 +32,29 @@ function saveImage() {
 }
 
 function pattern() {
-	background('skyblue');
+	background(51);
 
-	for (let x = 0; x <= width; x += columnSlider.value()) {
+	var w = wSlider.value(); 
+	var h = w; 
 
-		let r = random(300);
-		let g = random(greenSlider.value());
-		let b = random(200, 350);
-		
-		// size
-		let s = random(40, 120);  
-		let y = random(s/2, height - s/2);
+	for (let x = 0; x <= width; x += w) {
+		for (let y = 0; y <= height; y += h) {
 
-		// self portrait
-		fill(r, g, b);
-		// face
-		ellipse(x, y, s); 
+			let r = random(0, 100); // red
+			let g = random(0, 150); // green
+			let b = random(150, 255); 
 
-		fill('yellow');
-		// right eye
-		ellipse(x + random(eyeSlider.value()), y - 10, s/4 + random(5)); 
-		// left eye
-		ellipse(x - 20, y - 10, s/4); 
+			if (y > heightSlider.value()) {
+				r += random(100, 200);
+				g += random(50, 150);
+				b -= random(100, 200);
+			}
 
-		rect(x - 10, y + 20, s/2, s/8, s/16);
-
-
-		// pattern
-		stroke(r, g, b);
-		strokeWeight(6);
-		line(x, 0, x + 100, height);
-		line(x + 100, 0, x, height);
-		noFill();
-		ellipse(x + 50, random(200 - circleSlider.value(), 200 + circleSlider.value()), 40);
+			fill(r, g, b);				
+			
+			rect(x + random(10), y + random(10), w + random(10), h/2 + random(200));
+		}
 	}
 
+	
 }
