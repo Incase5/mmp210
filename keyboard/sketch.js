f81d82d2d349a5f4699ce3d8f518b8bab3a5ac54
Incase5 <jeremybaut2>
2020-05-12 19:39:56 -0400

    update

--- keyboard/sketch.js
+++ keyboard/sketch.js
@@ -1,49 +1,35 @@
 /*
 Jeremy Bautista
-Sound Example
+Keyboard Sampler
 */
 
-var drum1;
 var song;
 
-function preload() {
-	drum1 = loadSound("drum_1.wav");
+
+function preload(){
 	song = loadSound("Jon_E_Alpha_-_09_-_Heavy_Day.mp3");
 }
 
-function setup() {
+function setup(){
 	createCanvas(640, 360);
-	drum1.playMode("restart"); 
 }
 
-function draw() {
-	if (song.isPlaying()) {
+function draw(){
+	if (song.isPlaying()){
 		background('skyblue');
-	} else {
+	}
+	else {
 		background('black');
 	}
-
-	var vol = map(mouseY, 0, height, 0, 1);
-	song.setVolume(vol);
-	drum1.setVolume(vol);
-
-	noStroke();
-	fill('orange');
-	rect(0, mouseY, width, 2);
-
-	var pan = map(mouseX, 0, width, 0.5, 2);
-	song.pan(pan);
-
-	rect(mouseX, 0, 2, height);
 }
 
-function mousePressed() {
-	drum1.play();
-
-	if (song.isPlaying()) {
-		song.pause();
-
-	} else {
-		song.play();
+function keyPressed(){
+	if (keyCode == 32){
+		if (song.isPlaying()){
+			song.pause();
+		}
+		else{
+			song.play();
+		}
 	}
 }
