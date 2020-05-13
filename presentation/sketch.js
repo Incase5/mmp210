8e80d28bc639668acadc3d6e5b3375b8c995d582
Incase5 <jeremybaut2>
2020-05-13 12:14:40 -0400

    update

---
+++ presentation/sketch.js
@@ -0,0 +1,30 @@
+/*
+Jeremy Bautista
+Final Project Pitch
+*/
+
+var currentSlide = 0;
+var nextButtonX = 850;
+var nextButtonY = 680;
+var nextButtonW = 100;
+var nextButtonH = 33;
+
+function setup(){
+	createCanvas(960, 720);
+}
+
+function draw(){
+	background('skyblue');
+
+	text('slide' + currentSlide, 100, 100);
+
+	fill('white');
+	stroke('black');
+
+	rect(nextButtonX, nextButtonY, nextButtonW, nextButtonH);
+	fill('black');
+	textSize(20);
+	noStroke();
+	textAlign(CENTER, CENTER);
+	text("Next", nextButtonX, nextButtonY, nextButtonW, nextButtonH);
+}
