939acdf640b9bbcbb069cd6316d9ec4068c660f3
Incase5 <jeremybaut2>
2020-05-22 11:46:35 -0400

    Update

--- final/sketch.js
+++ final/sketch.js
@@ -1,79 +1,131 @@
 /*
-Jeremy Bautista
+Jeremy Bautista 
 Final Project
 */
 
-function setup(){
-	createCanvas(640, 360);
-}
+var s1;
+var s2;
+var s3;
+var s4;
+var s5;
+var s6;
+var s7;
 
-function draw(){
-	  var w = width / notes.length;
- 	 for (var i = 0; i < notes.length; i++) {
-    var x = i * w;
-    // If the mouse is over the key
-    if (mouseX > x && mouseX < x + w && mouseY < height) {
-      // If we're clicking
-      if (mouseIsPressed) {
-        fill(100,255,200);
-      // Or just rolling over
-      } else {
-        fill(127);
-      }
-    } else {
-      fill(200);
-    }
-
-    // Draw the key
-    rect(x, 0, w-1, height-1);
-  }
-
-}
-
-
-/*
-var drum1;
-var song;
-
-function preload() {
-	drum1 = loadSound("drum_1.wav");
-	song = loadSound("Jon_E_Alpha_-_09_-_Heavy_Day.mp3");
+function preload(){
+	s1 = loadSound("sound_1.mp3");
+	s2 = loadSound("sound_2.mp3");
+	s3 = loadSound("sound_3.mp3");
+	s4 = loadSound("sound_4.mp3");
+	s5 = loadSound("sound_5.mp3");
+	s6 = loadSound("sound_6.mp3");
+	s7 = loadSound("sound_7.mp3");
 }
 
 function setup() {
-	createCanvas(640, 360);
-	drum1.playMode("restart"); 
+	createCanvas(600, 400);
+	noStroke();
 }
 
 function draw() {
-	if (song.isPlaying()) {
-		background('skyblue');
-	} else {
-		background('black');
+
+	background('skyblue');
+	stroke("black");
+	strokeWeight(5);
+
+	rect(5, 5, 55, 250);	//note 1
+	rect(60, 5, 60, 250);	//note 2
+	rect(120, 5, 60, 250);	//note 3
+	rect(180, 5, 60, 250);	//note 4
+	rect(240, 5, 60, 250);	//note 5
+	rect(300, 5, 60, 250);	//note 6
+	rect(360, 5, 60, 250);	//note 7
+
+	if (s1.isPlaying()){
+		rect(5, 10, 55, 250);
 	}
 
-	var vol = map(mouseY, 0, height, 0, 1);
-	song.setVolume(vol);
-	drum1.setVolume(vol);
+	if (s2.isPlaying()){
+		rect(60, 10, 60, 250);
+	}
 
-	noStroke();
-	fill('orange');
-	rect(0, mouseY, width, 2);
+	if (s3.isPlaying()){
+		rect(120, 10, 60, 250);
+	}
 
-	var pan = map(mouseX, 0, width, 0.5, 2);
-	song.pan(pan);
+	if (s4.isPlaying()){
+		rect(180, 10, 60, 250);
+	}
 
-	rect(mouseX, 0, 2, height);
-}
+	if (s5.isPlaying()){
+		rect(240, 10, 60, 250);
+	}
 
-function mousePressed() {
-	drum1.play();
+	if (s6.isPlaying()){
+		rect(300, 10, 60, 250);
+	}
 
-	if (song.isPlaying()) {
-		song.pause();
-
-	} else {
-		song.play();
+	if (s7.isPlaying()){
+		rect(360, 10, 60, 250);
 	}
 }
-*/
+
+function keyPressed(){
+	if (keyCode == 81){	//q button
+		s1.play();
+	}
+
+	if (keyCode == 87){	//w button
+		s2.play();
+	}
+
+	if (keyCode == 69){	//e button
+		s3.play();
+	}
+
+	if (keyCode == 82){	//r button
+		s4.play();
+	}
+
+	if (keyCode == 84){	//t button
+		s5.play();
+	}
+
+	if (keyCode == 89){	//y button
+		s6.play();
+	}
+
+	if (keyCode == 85){	//u button
+		s7.play();
+	}
+}
+
+function keyReleased(){
+
+	if (keyCode == 81){	//q button
+		s1.stop();
+	}
+
+	if (keyCode == 87){	//w button
+		s2.stop();
+	}
+
+	if (keyCode == 69){	//e button
+		s3.stop();
+	}
+
+	if (keyCode == 82){	//r button
+		s4.stop();
+	}
+
+	if (keyCode == 84){	//t button
+		s5.stop();
+	}
+
+	if (keyCode == 89){	//y button
+		s6.stop();
+	}
+
+	if (keyCode == 85){	//u button
+		s7.stop();
+	}
+}
